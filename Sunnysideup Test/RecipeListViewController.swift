8ae3c9d69a9001fc5cb400f6e5afc15ac15ed453
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-04-11 15:40:33 +0700

    fix card height and layout

--- Sunnysideup Test/RecipeListViewController.swift
+++ Sunnysideup Test/RecipeListViewController.swift
@@ -21,10 +21,26 @@ class RecipeListViewController: ViewController {
                     Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
                     Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
                     Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
+                ]),
+        OneDayMenu(dayNumber: 3,
+                   dayMenuList: [
+                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol", menuDescription: "Deskripsi tempe mendol apa yaaa"),
+                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "Wow salted egg tempe, enak sekali!!"),
+                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "tempe dengan kecap ABC ulala")
+                ]),
+        OneDayMenu(dayNumber: 4,
+                   dayMenuList: [
+                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
+                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
+                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
                 ])
     ]
     
-    @IBOutlet weak var AllRecipeTableView: UITableView!
+    @IBOutlet weak var AllRecipeTableView: UITableView! {
+        didSet {
+            AllRecipeTableView.rowHeight = 210
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,7 +79,7 @@ extension RecipeListViewController: UITableViewDataSource {
         let recipe = weeklyRecipes[indexPath.section].dayMenuList![indexPath.row]
 
         cell.RecipeNameLabel.text = recipe.menuTitle
-        cell.RecipeTimeCalorieLabel.text = "\(recipe.menuCookTime) | \(recipe.menuCalorie)"
+        cell.RecipeTimeCalorieLabel.text = "\(recipe.menuCookTime!) | \(recipe.menuCalorie!)"
         cell.RecipeDescriptionLabel.text = recipe.menuDescription
         cell.RecipeImageView.image = UIImage(named: recipe.menuImageName!)
         
