0bd1720bdd361d80a4a0389d7a32efd47dc73d55
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-04-11 13:45:59 +0700

    remove nested uitableview

--- Sunnysideup Test/RecipeListViewController.swift
+++ Sunnysideup Test/RecipeListViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class RecipeListViewController: ViewController {
     
-    var mealArray: [Menu] = [
-        Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol"),
-        Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg"),
-        Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg")
+    var weeklyMenu: [OneDayMenu] = [
+        OneDayMenu(dayNumber: 1,
+                   dayMenuList: [
+                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol"),
+                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg"),
+                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg")
+                ]),
+        OneDayMenu(dayNumber: 2,
+                   dayMenuList: [
+                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob"),
+                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick"),
+                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward")
+                ])
     ]
+    
+    @IBOutlet weak var AllRecipeTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
