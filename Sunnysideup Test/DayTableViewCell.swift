d6bfba2fcfa7f89278d84323e7577cd2e6d44b9d
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-04-11 12:50:26 +0700

    add DayTableViewCell and RecipeTableViewCell

---
+++ Sunnysideup Test/DayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DayTableViewCell.swift
+//  Sunnysideup Test
+//
+//  Created by Nurul Srianda Putri on 11/04/22.
+//
+
+import UIKit
+
+class DayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
